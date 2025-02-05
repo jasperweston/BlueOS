edd4915e1e24909ed6e1746f14c4145d42030279
jweston <jasperweston01@gmail.com>
2025-02-05 13:33:31 +0800

    Fixed new line error

--- core/services/ardupilot_manager/flight_controller_detector/linux/OBAL.py
+++ core/services/ardupilot_manager/flight_controller_detector/linux/OBAL.py
@@ -70,6 +70,3 @@ class OBAL_Pi4(OBAL):
             raise RuntimeError("OBAL is not currently supported on RPi5")
             return False
         return all(self.check_for_i2c_device(bus, address) for address, bus in self.devices.values())
-    
-
-    
